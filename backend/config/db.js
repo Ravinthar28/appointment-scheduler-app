850f4598761e1abdacc6bf0c6d6f54704706d5c9
Ravinthar28 <ravintharravinthar2005@gmail.com>
2025-07-31 00:25:19 +0530

    new ui added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
 const db_uri = "mongodb://localhost:27017/db";
 
+// process.env.mongoDb_URI
 const connectDB = async ()=> {
     try{
         await mongoose.connect(db_uri,{
---
+++ frontend/app/app/(flash-screen)/new_style.ts
@@ -0,0 +1,65 @@
+
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'flex-end', // Aligns content to the bottom
+    alignItems: 'center',
+    paddingBottom: 50, // Adjust as needed to move content up from the bottom
+    width: '100%',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#ccc', // A slightly lighter color for subtitle
+    textAlign: 'center',
+    marginBottom: 40,
+    lineHeight: 24,
+  },
+  loginButton: {
+    backgroundColor: '#4A6296', // A darker blue similar to the image
+    paddingVertical: 15,
+    paddingHorizontal: 80,
+    borderRadius: 30,
+    marginBottom: 15,
+    width: '80%', // Adjust width as needed
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 8,
+  },
+  registerButton: {
+    backgroundColor: '#4A6296', // Same color as login button
+    paddingVertical: 15,
+    paddingHorizontal: 80,
+    borderRadius: 30,
+    width: '80%', // Adjust width as needed
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
