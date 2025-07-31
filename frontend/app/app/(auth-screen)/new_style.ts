13922003150a825ceaf9b30e9daa22b5afbd1e2c
Ravinthar28 <ravintharravinthar2005@gmail.com>
2025-07-31 23:21:12 +0530

    working on register screen

--- frontend/app/app/(auth-screen)/new_style.ts
+++ frontend/app/app/(auth-screen)/new_style.ts
@@ -6,5 +6,36 @@ export const register_styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingVertical:60,
+    paddingHorizontal:20
   },
+  innerContainer:{
+    width:"100%",
+    height:"100%",
+  },
+  pageTitle:{
+    display:"flex",
+    flexDirection:"row",
+    alignItems:"center",
+    marginVertical:20
+  },
+  
+  pageTitleIcon:{
+    width:"20%",
+    display:"flex",
+    alignItems:"center",
+    cursor:"pointer"
+  },
+  pageTitleContent:{
+    color:"white",
+    fontFamily:"sans-serif",
+    fontSize:30,
+    marginLeft:50,
+    fontWeight:"bold"
+  },
+  personalInfoTitle:{
+    color:"white",
+    fontSize:20,
+    fontWeight:"bold"
+  }
 })
--- frontend/app/app/(auth-screen)/register_new.tsx
+++ frontend/app/app/(auth-screen)/register_new.tsx
@@ -13,16 +13,33 @@ import {
 } from "react-native";
 
 import { register_styles } from "./new_style";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-export default function Register(){
-    return(
-        <LinearGradient
-              colors={['#2D3F75', '#3A508C', '#5C7FB7', '#7D9DCF']} // 
-              style={register_styles.container}
-              start={{ x: 0.5, y: 0 }} // Starts from the top-center
-              end={{ x: 0.5, y: 1 }}   // Ends at the bottom-center
-            >
-                
-            </LinearGradient>
-    )
+
+export default function Register() {
+  return (
+    <LinearGradient
+      colors={["#2D3F75", "#3A508C", "#5C7FB7", "#7D9DCF"]} //
+      style={register_styles.container}
+      start={{ x: 0.5, y: 0 }} // Starts from the top-center
+      end={{ x: 0.5, y: 1 }} // Ends at the bottom-center
+    >
+        <View style={register_styles.innerContainer}>
+            <View style={register_styles.pageTitle}>
+                <View style={register_styles.pageTitleIcon}>
+                  <FontAwesomeIcon icon={faArrowLeft} size={30} color="white" />
+                </View>
+                <View style={register_styles.pageTitleContent}>
+                    Create Account
+                </View>
+            </View>
+            <View>
+              <Text style={register_styles.personalInfoTitle}>
+                Personal Information
+              </Text>
+            </View>
+        </View>
+    </LinearGradient>
+  );
 }
