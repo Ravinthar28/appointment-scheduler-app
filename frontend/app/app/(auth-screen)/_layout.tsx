89dd414cfddfb9d84f6d14118b226445b344820a
Ravinthar28 <akashwolf0508@gmail.com>
2025-07-31 19:52:03 +0530

    update layout

--- frontend/app/app/(auth-screen)/_layout.tsx
+++ frontend/app/app/(auth-screen)/_layout.tsx
@@ -4,8 +4,8 @@ export default function AuthLayout(){
     return(
         <Stack>
             <Stack.Screen name="register_new" options={{title:"Login",headerShown:false}} />
-            <Stack.Screen name="login" options={{title:"Login"}}/>
-            <Stack.Screen name ="register" options={{title:"Register"}} />
+            <Stack.Screen name="login" options={{title:"Login",headerShown:false}}/>
+            <Stack.Screen name ="register" options={{title:"Register",headerShown:false}} />
         </Stack>
     )
 }
