1c7b6b76c45d06cd558c5632173b7363547cd3a4
Ravinthar28 <131782227+Ravinthar28@users.noreply.github.com>
2025-08-20 14:13:42 +0530

    apiUrl code changed

--- frontend/app/app/apiUrl.js
+++ frontend/app/app/apiUrl.js
@@ -3,22 +3,10 @@
 
 //export const baseUrl = "http://localhost:3000"
 
-<<<<<<< HEAD
 export const baseUrl = "http://localhost:3000"
-=======
-<<<<<<< HEAD
-export const baseUrl = "http://192.168.86.146:3000"
-=======
-export const baseUrl = "http://10.248.84.146:3000"
->>>>>>> 9863eeeaa92b2df49742f51a51f30210883bce3e
-
->>>>>>> 0031f9d52bdab56ac88f47161745b3c1b2739258
 
 
 
 // college URL
-<<<<<<< HEAD
+
 //export const baseUrl = "https://appointment-scheduler-15l0.onrender.com"
-=======
-// export const baseUrl = "https://appointment-scheduler-15l0.onrender.com"
->>>>>>> 0031f9d52bdab56ac88f47161745b3c1b2739258
