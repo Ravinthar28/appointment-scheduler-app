936b8d37700feae1a7dd60e4822820136172c7bb
Ravinthar28 <akashwolf0508@gmail.com>
2025-08-20 13:04:41 +0530

    date and time fixed

--- frontend/app/app/apiUrl.js
+++ frontend/app/app/apiUrl.js
@@ -1,12 +1,11 @@
 
 // BASE URL FOR API
-<<<<<<< HEAD
+
 //export const baseUrl = "http://localhost:3000"
-=======
+
 export const baseUrl = "http://192.168.86.146:3000"
->>>>>>> a90e1eaa1fe6c8f21ca9451fd3b13686c582d240
 
 
 
 // college URL
-export const baseUrl = "https://appointment-scheduler-15l0.onrender.com"
+//export const baseUrl = "https://appointment-scheduler-15l0.onrender.com"
